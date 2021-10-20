42af2b6a0ea600be3d3f53d353dbf657eed86c2a
SethEvry <92412142+SethEvry@users.noreply.github.com>
2021-10-20 09:56:47 -0400

    add toddleColor function

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -2,6 +2,8 @@
 const userName = document.getElementById("name");
 const title = document.getElementById("title");
 const other = document.getElementById("other-job-role");
+const design = document.getElementById("design");
+const color = document.getElementById("color");
 //function declarations
 const addOther = () => {
   if (title.value === "other") {
@@ -10,10 +12,38 @@ const addOther = () => {
     other.style.display = "none";
   }
 };
+const toggleColor = () => {
+  const options = color.children;
+  if (design.value === "Select Theme") {
+    color.setAttribute("disabled", true);
+  } else {
+    color.removeAttribute("disabled");
+    for (let option of options) {
+      if (design.value !== option.getAttribute("data-theme")) {
+        option.style.display = "none";
+      } else {
+        option.style.display = "";
+      }
+    }
+  }
+};
+
 // start up methods and functions
 addOther();
 userName.focus();
-// Adds the job role box if other is selected
+toggleColor();
+
+// Event Handlers
+
+/**
+ * Adds job role text input when other is selected
+ */
 title.addEventListener("change", () => {
   addOther();
+});
+/**
+ * Enables shirt color selector when a design is chosen
+ */
+design.addEventListener("change", () => {
+  toggleColor();
 });
